c67a856ad21afad87a8016d6dfc6b766cef864db
D4NIEL-DIAZ <danieldiesta00@gmail.com>
2026-03-09 21:58:58 +0800

    adding  Query Scopes and Accessors and Mutators

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Scopes\VerifiedUsersScope;
 
 class User extends Authenticatable
 {
@@ -22,6 +23,7 @@ class User extends Authenticatable
         'full_name',  
         'email',
         'password',
+        'email_verified_at',
     ];
 
     /**
@@ -51,4 +53,42 @@ class User extends Authenticatable
     {
         return $this->hasOne(Address::class);
     }
+    
+        public static function booted(): void
+    {
+
+        static::addGlobalScope(new VerifiedUsersScope);
+
+        static::creating(function ($user) {
+            // Only set full_name if it's empty
+            if (empty($user->full_name) && !empty($user->first_name) && !empty($user->last_name)) {
+                $user->full_name = trim($user->first_name . ' ' . $user->last_name);
+            }
+
+            // Only set name if it's empty
+            if (empty($user->name)) {
+                $user->name = $user->full_name ?? ($user->first_name ?? 'Unknown');
+            }
+        });
+    }
+
+        public function getInitialsAttribute()
+    {
+        $firstInitial = strtoupper(substr($this->first_name, 0, 1));
+        $lastInitial = strtoupper(substr($this->last_name, 0, 1));
+
+        return $firstInitial . $lastInitial;
+    }
+    public function setLastNameAttribute($value)
+    {
+        $this->attributes['last_name'] = strtoupper($value);
+    }
+
+    public function scopeActiveSince($query, $date)
+    {
+        return $query->whereNotNull('email_verified_at')
+                    ->where('created_at', '>=', $date);
+    }
+
+    
 }
